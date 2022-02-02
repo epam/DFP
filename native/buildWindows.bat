b7d5c010d7c09608235f2db1212e4662bcf7f39b
Andrei Davydov <adavydov@deltixlab.com>
2022-02-03 02:29:51 +0300

    Native: Fix windows artifact placement

--- native/buildWindows.bat
+++ native/buildWindows.bat
@@ -3,7 +3,7 @@ cd buildx86
 cmake -G "Visual Studio 16 2019" -A Win32 -DVERSION_SUFFIX=%1 ../
 MSBuild /p:PlatformToolset=ClangCL /p:Platform=Win32 /p:Configuration=Release /t:Rebuild ./native.sln
 mkdir ..\binmath\Release\Windows\x86
-move /y Release\*math*.dll ..\bin\Release\Windows\x86
+move /y Release\*math*.dll ..\binmath\Release\Windows\x86
 mkdir ..\bin\Release\Windows\x86
 move /y Release\*.dll ..\bin\Release\Windows\x86
 mkdir ..\binDemo\Release\Windows\x86
@@ -16,7 +16,7 @@ cd buildamd64
 cmake -G "Visual Studio 16 2019" -A x64 -DVERSION_SUFFIX=%1 ../
 MSBuild /p:PlatformToolset=ClangCL /p:Platform=x64 /p:Configuration=Release /t:Rebuild ./native.sln
 mkdir ..\binmath\Release\Windows\amd64
-move /y Release\*math*.dll ..\bin\Release\Windows\amd64
+move /y Release\*math*.dll ..\binmath\Release\Windows\amd64
 mkdir ..\bin\Release\Windows\amd64
 move /y Release\*.dll ..\bin\Release\Windows\amd64
 mkdir ..\binDemo\Release\Windows\amd64
