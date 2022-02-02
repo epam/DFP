e8cbdc98a0e4fa875a3383b2d65d997e9675220d
Andrei Davydov <adavydov@deltixlab.com>
2022-02-02 19:00:35 +0300

    Native: place output library files to the separate 'binmath' folder.

--- native/buildWindows.bat
+++ native/buildWindows.bat
@@ -2,10 +2,12 @@ mkdir buildx86
 cd buildx86
 cmake -G "Visual Studio 16 2019" -A Win32 -DVERSION_SUFFIX=%1 ../
 MSBuild /p:PlatformToolset=ClangCL /p:Platform=Win32 /p:Configuration=Release /t:Rebuild ./native.sln
+mkdir ..\binmath\Release\Windows\x86
+move /y Release\*math*.dll ..\bin\Release\Windows\x86
 mkdir ..\bin\Release\Windows\x86
-copy /y Release\*.dll ..\bin\Release\Windows\x86
+move /y Release\*.dll ..\bin\Release\Windows\x86
 mkdir ..\binDemo\Release\Windows\x86
-copy /y EXAMPLES\Release\*.exe ..\binDemo\Release\Windows\x86
+move /y EXAMPLES\Release\*.exe ..\binDemo\Release\Windows\x86
 
 cd ..
 
@@ -13,8 +15,10 @@ mkdir buildamd64
 cd buildamd64
 cmake -G "Visual Studio 16 2019" -A x64 -DVERSION_SUFFIX=%1 ../
 MSBuild /p:PlatformToolset=ClangCL /p:Platform=x64 /p:Configuration=Release /t:Rebuild ./native.sln
+mkdir ..\binmath\Release\Windows\amd64
+move /y Release\*math*.dll ..\bin\Release\Windows\amd64
 mkdir ..\bin\Release\Windows\amd64
-copy /y Release\*.dll ..\bin\Release\Windows\amd64
+move /y Release\*.dll ..\bin\Release\Windows\amd64
 mkdir ..\binDemo\Release\Windows\amd64
-copy /y EXAMPLES\Release\*.exe ..\binDemo\Release\Windows\amd64
+move /y EXAMPLES\Release\*.exe ..\binDemo\Release\Windows\amd64
 cd ..
