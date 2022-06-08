73b9ccc4c4361d63a8f7b4f47275ff967a3fdc86
Andrei Davydov <andrei_davydau1@epam.com>
2022-06-08 20:12:50 +0300

    Java: Update loader.

--- java/dfp-math/src/main/java/com/epam/deltix/dfpmath/NativeMathImplLoader.java
+++ java/dfp-math/src/main/java/com/epam/deltix/dfpmath/NativeMathImplLoader.java
@@ -1,36 +1,25 @@
 package com.epam.deltix.dfpmath;
 
-import com.epam.deltix.utilities.ResourceLoader;
-
-import java.io.File;
-import java.nio.file.Paths;
-import java.util.Locale;
+import com.epam.deltix.utilities.*;
 
 class NativeMathImplLoader {
     public static void load() {
         try {
             String version = NativeMathImpl.version;
-
             final boolean isSnapshot = version.endsWith("-SNAPSHOT");
             if (isSnapshot)
                 version = version.substring(0, version.length() - "-SNAPSHOT".length());
 
-            String osName = System.getProperty("os.name");
-            String osArch = System.getProperty("os.arch");
+            VariablesMapper varMapper = new VariablesMapper(NativeMathImplLoader.class, version);
 
-            if (osName.toLowerCase(Locale.ROOT).contains("windows"))
-                osName = "Windows";
-            if (osName.toLowerCase(Locale.ROOT).contains("mac")) {
-                osName = "Darwin";
-                if (osArch.toLowerCase(Locale.ROOT).contains("arm64"))
-                    osArch = "aarch64";
-            }
-
-            final String packageName = NativeMathImplLoader.class.getPackage().getName();
+            final String unpackEnvVarName = varMapper.getPackageLast().toUpperCase() + "_UNPACK_ROOT";
+            String unpackPath = System.getenv(unpackEnvVarName);
+            if (unpackPath == null)
+                unpackPath = "$(TEMP)/$(PACKAGE)/$(VERSION)/$(ARCH)";
 
             ResourceLoader
-                .from(NativeMathImplLoader.class, "resources_" + packageName.replace('.', '_') + '/' + osName + '/' + osArch + "/*")
-                .to(Paths.get(System.getProperty("java.io.tmpdir"), packageName.replace('.', File.separatorChar), version, osArch).toString())
+                .from(NativeMathImplLoader.class, "resources_" + varMapper.getPackage().replace('.', '_') + '/' + varMapper.getOsName() + '/' + varMapper.getOsArch() + "/*")
+                .to(varMapper.substitute(unpackPath))
                 .alwaysOverwrite(isSnapshot)
                 .tryRandomFallbackSubDirectory(true)
                 .load();
