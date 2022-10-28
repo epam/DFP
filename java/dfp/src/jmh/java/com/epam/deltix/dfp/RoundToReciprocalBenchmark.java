a593dc69d0db5e0d4c4d4c61f9e2b830e341102b
Andrei Davydov <andrei_davydau1@epam.com>
2022-10-28 19:28:48 +0300

    Add RoundToReciprocalBenchmark.

---
+++ java/dfp/src/jmh/java/com/epam/deltix/dfp/RoundToReciprocalBenchmark.java
@@ -0,0 +1,113 @@
+package com.epam.deltix.dfp;
+
+import org.apache.commons.math3.random.MersenneTwister;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.math.RoundingMode;
+import java.security.SecureRandom;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import static com.epam.deltix.dfp.Decimal64Utils.MAX_SIGNIFICAND_DIGITS;
+import static com.epam.deltix.dfp.TestUtils.POWERS_OF_TEN;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(time = 3, iterations = 1)
+@Measurement(time = 3, iterations = 3)
+@State(Scope.Thread)
+public class RoundToReciprocalBenchmark {
+    private long[] decimalValues;
+    private int[] n;
+    private long[] mul;
+    public static int fixedSeed = 42 * 42 * 42 * 42 * 42;
+
+    @Setup
+    public void setUp() {
+        final Random random = new SecureRandom();
+
+        decimalValues = new long[1000];
+        n = new int[decimalValues.length];
+        mul = new long[decimalValues.length];
+
+        for (int i = 0; i < decimalValues.length; ++i) {
+            final int mantissaLen = random.nextInt(Decimal64Utils.MAX_SIGNIFICAND_DIGITS) + 1;
+            final long mantissa = random.nextLong() % POWERS_OF_TEN[mantissaLen];
+
+            final int exp = random.nextInt(20) - Decimal64Utils.MAX_SIGNIFICAND_DIGITS;
+
+            decimalValues[i] = Decimal64Utils.fromFixedPoint(mantissa, -exp);
+
+            n[i] = Math.abs(random.nextInt());
+
+            mul[i] = Decimal64Utils.divideByInteger(Decimal64Utils.ONE, n[i]);
+        }
+    }
+
+    @Benchmark
+    public void roundTowardsPositiveInfinity(Blackhole bh) {
+        for (int i = 0; i < decimalValues.length; ++i)
+            bh.consume(Decimal64Utils.roundTowardsPositiveInfinity(decimalValues[i], mul[i]));
+    }
+
+    @Benchmark
+    public void roundTowardsNegativeInfinity(Blackhole bh) {
+        for (int i = 0; i < decimalValues.length; ++i)
+            bh.consume(Decimal64Utils.roundTowardsNegativeInfinity(decimalValues[i], mul[i]));
+    }
+
+    @Benchmark
+    public void roundToNearestTiesAwayFromZero(Blackhole bh) {
+        for (int i = 0; i < decimalValues.length; ++i)
+            bh.consume(Decimal64Utils.roundToNearestTiesAwayFromZero(decimalValues[i], mul[i]));
+    }
+
+    @Benchmark
+    public void roundToNearestTiesToEven(Blackhole bh) {
+        for (int i = 0; i < decimalValues.length; ++i)
+            bh.consume(Decimal64Utils.roundToNearestTiesToEven(decimalValues[i], mul[i]));
+    }
+
+    @Benchmark
+    public void roundToReciprocalUp(Blackhole bh) {
+        for (int i = 0; i < decimalValues.length; ++i)
+            bh.consume(Decimal64Utils.roundToReciprocal(decimalValues[i], n[i], RoundingMode.UP));
+    }
+
+    @Benchmark
+    public void roundToReciprocalDown(Blackhole bh) {
+        for (int i = 0; i < decimalValues.length; ++i)
+            bh.consume(Decimal64Utils.roundToReciprocal(decimalValues[i], n[i], RoundingMode.DOWN));
+    }
+
+    @Benchmark
+    public void roundToReciprocalCeiling(Blackhole bh) {
+        for (int i = 0; i < decimalValues.length; ++i)
+            bh.consume(Decimal64Utils.roundToReciprocal(decimalValues[i], n[i], RoundingMode.CEILING));
+    }
+
+    @Benchmark
+    public void roundToReciprocalFloor(Blackhole bh) {
+        for (int i = 0; i < decimalValues.length; ++i)
+            bh.consume(Decimal64Utils.roundToReciprocal(decimalValues[i], n[i], RoundingMode.FLOOR));
+    }
+
+    @Benchmark
+    public void roundToReciprocalHalfUp(Blackhole bh) {
+        for (int i = 0; i < decimalValues.length; ++i)
+            bh.consume(Decimal64Utils.roundToReciprocal(decimalValues[i], n[i], RoundingMode.HALF_UP));
+    }
+
+    @Benchmark
+    public void roundToReciprocalHalfDown(Blackhole bh) {
+        for (int i = 0; i < decimalValues.length; ++i)
+            bh.consume(Decimal64Utils.roundToReciprocal(decimalValues[i], n[i], RoundingMode.HALF_DOWN));
+    }
+
+    @Benchmark
+    public void roundToReciprocalHalfEven(Blackhole bh) {
+        for (int i = 0; i < decimalValues.length; ++i)
+            bh.consume(Decimal64Utils.roundToReciprocal(decimalValues[i], n[i], RoundingMode.HALF_EVEN));
+    }
+}
