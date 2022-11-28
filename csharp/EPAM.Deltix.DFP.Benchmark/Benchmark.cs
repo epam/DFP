149b56a8b53ae50d6256b20f8e4e05fc5b199690
Andrei Davydov <andrei_davydau1@epam.com>
2022-11-28 13:22:33 +0300

    Fix .NET benchmarks - initial version

--- csharp/EPAM.Deltix.DFP.Benchmark/Benchmark.cs
+++ csharp/EPAM.Deltix.DFP.Benchmark/Benchmark.cs
@@ -1,65 +1,54 @@
 using System;
 using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Attributes.Columns;
-using BenchmarkDotNet.Attributes.Exporters;
-using BenchmarkDotNet.Attributes.Jobs;
 using BenchmarkDotNet.Running;
 
 namespace EPAM.Deltix.DFP.Benchmark
 {
-	[ClrJob(isBaseline: true), CoreJob]
-	[RPlotExporter, RankColumn]
 	public class Benchmark
 	{
-		private Decimal64 a1, b1;
-		private Decimal a2, b2;
+        [Params(509104287)]
+        public int randomSeed { get; set; }
+
+        [Params(1000_000)]
+		public int N { get; set; }
+
+		public Decimal64[] values;
 
 		[GlobalSetup]
 		public void Setup()
 		{
-			Random random = new Random();
-			Double a = random.NextDouble();
-			Double b = random.NextDouble();
-			a1 = Decimal64.FromDouble(a);
-			b1 = Decimal64.FromDouble(b);
-			a2 = (Decimal) a;
-			b2 = (Decimal) b;
+			values = new Decimal64[N + 1];
+			var generator = new RandomDecimalsGenerator(randomSeed);
+
+			for (int i = 0; i < values.Length; ++i)
+				values[i] = generator.NextX();
 		}
 
 		[Benchmark]
-		public Decimal64 AddDecimal64()
+		public void AddDecimal64()
 		{
-			return a1 + b1;
+			for (int i = 0; i < N; ++i)
+			{
+				var c = values[i] + values[i + 1];
+			}
 		}
 
 		[Benchmark]
-		public Decimal AddSystem()
+		public void MultiplyDecimal64()
 		{
-			return a2 + b2;
+			for (int i = 0; i < N; ++i)
+			{
+				var c = values[i] * values[i + 1];
+			}
 		}
 
 		[Benchmark]
-		public Decimal64 MultiplyDecimal64()
+		public void DivisionDecimal64()
 		{
-			return a1 * b1;
-		}
-
-		[Benchmark]
-		public Decimal MultiplySystem()
-		{
-			return a2 * b2;
-		}
-
-		[Benchmark]
-		public Decimal64 DivisionDecimal64()
-		{
-			return a1 / b1;
-		}
-
-		[Benchmark]
-		public Decimal DivisionSystem()
-		{
-			return a2 / b2;
+			for (int i = 0; i < N; ++i)
+			{
+				var c = values[i] / values[i + 1];
+			}
 		}
 
 		public static void Main(String[] args)
